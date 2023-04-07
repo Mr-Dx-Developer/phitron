aa716258f85e89ccb4925b28556cbff03bd56e20
Tanvir Anjum Neon <taneon98@gmail.com>
2023-04-07 10:21:30 +0600

    done

--- C_3_Loop/many_time_use_LOOP.c
+++ C_3_Loop/many_time_use_LOOP.c
@@ -2,10 +2,13 @@
 
 int main()
 {
-    int i;
-    for (i=1; i<=5; i=i+1)
+    int N;
+    scanf("%d", &N );
+
+
+    for (int i =1; i<=N; i=i+1)
     {
-        printf("Hello\n");
+        printf("I Love Practice\n");
     }
     
     return 0;
