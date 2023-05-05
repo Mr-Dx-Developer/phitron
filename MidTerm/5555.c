e8128d8e290836b4322ada3c28eeb4660f7c1ed3
Tanvir Anjum Neon <taneon98@gmail.com>
2023-05-05 20:41:04 +0600

    k

--- MidTerm/5555.c
+++ MidTerm/5555.c
@@ -5,6 +5,19 @@ int main() {
     char s[1000];
     scanf("%s", s);
 
+    int l = strlen(s);
+
+    int ac[26] = {0};
+
+    for (int i = 0; i < l; i++) {
+        ac[s[i] - 'a']++;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        printf("%c - %d\n", 'a' + i, ac[i]);
+        
+    }
+
     
     
 
